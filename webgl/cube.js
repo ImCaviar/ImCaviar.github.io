5b1ba76b4f14122f943776b2b4e51461f1493c38
Liu Xinyu <40049653+ImCaviar@users.noreply.github.com>
2019-04-12 16:07:04 +0800

    Add files via upload

---
+++ webgl/cube.js
@@ -0,0 +1,86 @@
+document.write("<script language=javascript src='../three/three.js'></script>");
+var head = 'data:text/csv;charset=utf-8,';
+var excel = '';
+function drawCube() {
+    //设置场景大小
+    var width = 400;
+    var height = 400;
+    var scene = new THREE.Scene();
+
+    //设置相机参数,相机开角，成像比例，相机能拍摄到的最近距离，相机能拍摄到的最远距离
+    var camera = new THREE.PerspectiveCamera(45,width/height,1,1000);
+
+    //新建渲染
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(0xffffff);
+    renderer.setSize(width,height);
+    renderer.shadowMap.enabled = true;//支持阴影效果
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;//阴影类型
+
+    //添加一个立方体
+    var box = new THREE.Mesh(
+        new THREE.BoxGeometry(2,2,2),
+        new THREE.MeshLambertMaterial({
+            color:0xffdd00
+        }));
+
+    //把立方体放到原点位置
+    box.position.set(0,0,0);
+
+    //在场景中添加立方体
+    scene.add(box);
+
+    //将相机放到立方体斜上方并对着立方体
+    camera.position.set(5,5,5);
+    camera.lookAt(box.position);
+
+    //设置光源
+    var light = new THREE.DirectionalLight(0xffffff,1);
+    light.position.set(10,50,100);
+    scene.add(light);
+
+    //渲染场景
+    document.getElementById('cube').appendChild(renderer.domElement);
+    renderer.render(scene,camera);
+
+    //转换为图片
+    var d = renderer.domElement.toDataURL();
+    let img = document.createElement('img');
+    img.src = d;
+    //console.log(d);
+
+    //复制图片并计算像素值的和
+    img.onload = function () {
+        //创建画布
+        var canvas = document.getElementById('can2');
+        var ctx = canvas.getContext('2d');
+        ctx.drawImage(img,0,0,width,height);
+        var data = ctx.getImageData(0,0,width,height).data;
+        //console.log(data);
+
+        //导出为csv文件
+        //for (var i=0; i<300; i++){
+        //    excel += 'R'+i.toString()+'\,G'+i.toString()+'\,B'+i.toString()+'\,A'+i.toString()+'\,';
+        //}
+        //excel += '\n';
+        var numr = 0;
+        var numg = 0;
+        for (var i=0; i<height; i++){
+            for (var j=0; j<width; j++){
+                //excel += data[(i*300+j)*4].toString()+'\,'+data[(i*300+j)*4+1].toString()+'\,'+data[(i*300+j)*4+2].toString()+'\,'+data[(i*300+j)*4+3].toString()+'\,';
+                if (data[(i*width+j)*4+2]!=255){
+                    numr += data[(i*width+j)*4];
+                    numg += data[(i*width+j)*4+1];
+                }
+            }
+            //excel += '\n';
+        }
+        document.getElementById('number2R').value = numr;
+        document.getElementById('number2G').value = numg;
+    }
+}
+drawCube();
+//function clickDownload(aLink) {
+//    aLink.href = head + encodeURIComponent(excel);
+//    aLink.click();
+//}
---
+++ webgl/texture.js
@@ -0,0 +1,75 @@
+document.write("<script language=javascript src='../three/three.js'></script>");
+function drawTexture() {
+    //设置场景大小
+    var width = 400;
+    var height = 400;
+    var scene = new THREE.Scene();
+
+    //初始化渲染器
+    var renderer = new THREE.WebGLRenderer({
+        antialias:true
+    });
+    renderer.setSize(width,height);
+    renderer.setClearColor(0xffffff);
+
+    //初始化对象，立方体
+    var loader = new THREE.TextureLoader();
+    loader.load('img/stone.jpg',function (texture) {
+        var material = new THREE.MeshLambertMaterial({
+            map:texture
+        });
+        var geometry = new THREE.BoxGeometry(15,15,10);
+        var mesh = new THREE.Mesh(geometry,material);
+        mesh.position.set(0,0,0);
+        scene.add(mesh);
+
+        //设置相机参数
+        var camera = new THREE.PerspectiveCamera(45,width/height,0.1,1000);
+        camera.position.set(15,20,25);
+        camera.lookAt(mesh.position);
+
+        //初始化光源，漫反射
+        var light = new THREE.AmbientLight(0xffffff);
+        light.position.set(60,100,80);
+        scene.add(light);
+
+        //渲染成像
+        document.getElementById('texture').appendChild(renderer.domElement);
+        renderer.render(scene,camera);
+
+        //转换为图片
+        var d = renderer.domElement.toDataURL();
+        let img = document.createElement('img');
+        img.src = d;
+        //console.log(d);
+
+        //复制图像并计算像素值的和
+        img.onload = function () {
+            //创建画布
+            var canvas = document.getElementById('can3');
+            var ctx = canvas.getContext('2d');
+            ctx.drawImage(img,0,0,width,height);
+            var data = ctx.getImageData(0,0,width,height).data;
+            console.log(data);
+
+            //统计RGB数值总数
+            var numr = 0;
+            var numg = 0;
+            var numb = 0;
+            for (var i = 0; i < 400; i++){
+                for (var j = 0; j < 400; j++){
+                    if (data[(i*400+j)*4]!=255 || data[(i*400+j)*4+1]!=255 || data[(i*400+j)*4+2]!=255){
+                        numr += data[(i*400+j)*4];
+                        numg += data[(i*400+j)*4+1];
+                        numb += data[(i*400+j)*4+2];
+                    }
+                }
+            }
+            document.getElementById('number3R').value = numr;
+            document.getElementById('number3G').value = numg;
+            document.getElementById('number3B').value = numb;
+        }
+    });
+
+}
+drawTexture();
